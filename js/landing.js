d6d4155a3af1f9ee9b9cf77967f818c1163f6b48
sherri010 <aminshahidi.sh@gmail.com>
2016-11-20 22:03:06 -0800

    redirects after 200 login

--- js/landing.js
+++ js/landing.js
@@ -1,35 +1,37 @@
 $(document).ready(function(){
-// the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-// $("#login").on('click',function(){
-//    $.ajax({
-//      method:"GET",
-//      url:"http://localhost:3000/users/sign_in",
-//      success:function(data){
-//        console.log(data);
-//
-//      }
-//    });
-//
-//
-// })
-
 
  $('.modal').modal();
-$('#login').on("submit",function(e){
- e.preventDefault();
- var form_data = $("#login").serialize();
- $.ajax({
-   method:"post",
-   url:"http://localhost:3000/users/sign_in.json",
-   data:{user:{email:"sherri@gmail.com",password:"asdfgh"}},
-   success:function(data){
-     $('#modal1').modal('close');
-      console.log(data)
-   },
-   error:function(err){
-     console.log(err);
-   }
- });
+ $('#login').on("submit",function(e){
+   e.preventDefault();
+   var form_data = $("#login").serialize();
+   $.ajax({
+     method:"post",
+     url:"http://localhost:3000/users/sign_in.json",
+     data:{user:{email:"sherri@gmail.com",password:"asdfgh"}},
+     success:function(data){
+       $('#modal1').modal('close');
+        console.log(data);
+        sessionStorage.setItem("auth_token", data.auth_token);
+        sessionStorage.setItem("current_user", data);
+        window.location.replace("dashboard.html");
+     },
+     error:function(err){
+       console.log(err);
+     }
+   });
+  });
 
-});
+  $('#signup').on("submit",function(e){
+    e.preventDefault();
+    $.ajax({
+      method:"POST",
+      url:"http://localhost:3000/users",
+      data:{name:$("#new_name").val(),email:$("#new_email").val(),password:$("#new_password").val()},
+      success:function(data){
+        $('#modal2').modal('close');
+         console.log(data)
+      }
+    });
+   });
+
 });
